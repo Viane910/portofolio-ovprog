785c8fa8baaf2700557be9e211e38c9617389315
Jjoongrami910 <doktaviane910@gmail.com>
2025-08-31 16:11:00 +0700

    .

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import HomePage from './script/pages/home/home-page.jsx';
 import AboutPage from './script/pages/about/about-page.jsx';
 import PortofolioPage from './script/pages/portofolio/portofolio.jsx';
@@ -6,14 +6,12 @@ import ContactPage from './script/pages/contact/contact-page.jsx';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/portofolio" element={<PortofolioPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="*" element={<div>404 - Page Not Found</div>} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/portofolio" element={<PortofolioPage />} />
+      <Route path="/contact" element={<ContactPage />} />
+      <Route path="*" element={<div>404 - Page Not Found</div>} />
+    </Routes>
   );
 }
